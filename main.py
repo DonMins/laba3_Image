b3ef3efc4c6db303ee2ecf510cc6137da121abea
DonMins <maks000156@yandex.ru>
2020-03-14 16:10:42 +0400

    на 85% готово

--- main.py
+++ main.py
@@ -7,175 +7,79 @@ class cv3:
         pass
 
     @classmethod
-    def erodeIsTrue(self,type, img, I, J , center):
-        threshold = 255
-        if type == 'cross':
-            for i in range(1, center + 1):
-                bool = img[I, J + i] == threshold and img[I, J - i] == threshold and img[I + i, J] == threshold and \
-                       img[I - i, J] == threshold
-                if (bool == False):
-                    return False
-            return True
-
-        if type == "square":
-            if center == 1:
-                bool = img[I, J + 1] == threshold and  img[I, J - 1] == threshold and img[
-                    I + 1, J] == threshold and img[
-                           I - 1, J] == threshold or img[I - 1, J - 1] == threshold and img[
-                           I - 1, J + 1] == threshold and \
-                       img[I + 1, J - 1] == threshold and img[I + 1, J + 1] == threshold
-                if (bool == False):
-                    return False
-                return True
-
-            if center == 2:
-                bool = \
-                    img[I, J + 1] == threshold \
-                    and img[I, J - 1] == threshold \
-                    and img[I + 1, J] == threshold \
-                    and img[I - 1, J] == threshold \
-                    and img[I - 1, J - 1] == threshold \
-                    and img[I - 1, J + 1] == threshold \
-                    and img[I + 1, J - 1] == threshold \
-                    and img[I + 1, J + 1] == threshold \
-                    and img[I, J + 2] == threshold \
-                    and img[I, J - 2] == threshold \
-                    and img[I + 2, J] == threshold \
-                    and img[I - 2, J] == threshold \
-                    and img[I - 1, J - 2] == threshold \
-                    and img[I - 1, J + 2] == threshold \
-                    and img[I + 2, J - 2] == threshold \
-                    and img[I + 2, J + 2] == threshold \
-                    and img[I + 2, J - 1] == threshold \
-                    and img[I + 2, J + 1] == threshold \
-                    and img[I + 1, J + 2] == threshold \
-                    and img[I + 1, J - 2] == threshold \
-                    and img[I - 2, J + 2] == threshold \
-                    and img[I - 2, J - 2] == threshold \
-                    and img[I - 2, J + 1] == threshold \
-                    and img[I - 2, J - 1] == threshold
-
-                if (bool == False):
-                    return False
-                return True
+    def erodeIsTrue(cls,img, mask, cenH, cenW):
+        p = img.shape
+        width = p[1]
+        height = p[0]
+        flag = True
+        for i in range(height):
+            for j in range(width):
+                    if  img[i, j] != mask[i, j]:
+                        return False
+        return True
 
     @classmethod
-    def dilateIsTrue(self,type, img, I, J , center):
-        threshold = 255
-        if type == 'cross':
-            for i in range(1, center + 1):
-                bool = img[I, J + i] == threshold or img[I, J - i] == threshold or img[I + i, J] == threshold or \
-                       img[I - i, J] == threshold
-                if (bool == False):
-                    return False
-            return True
+    def dilateIsTrue(cls,img, mask, cenH, cenW):
+        p = img.shape
+        width = p[1]
+        height = p[0]
+        for i in range(height):
+            for j in range(width):
+                if img[i, j] == mask[i, j]:
+                    return True
+        return False
 
-        if type == "square":
-            if center == 1:
-                bool = img[I, J + 1] == threshold or img[I, J - 1] == threshold or img[
-                    I + 1, J] == threshold or img[
-                           I - 1, J] == threshold or img[I - 1, J - 1] == threshold or img[
-                           I - 1, J + 1] == threshold or\
-                       img[I + 1, J - 1] == threshold or img[I + 1, J + 1] == threshold
+    @classmethod
+    def preparation(cls,img, mask):
+        p = img.shape
+        pM = mask.shape
+        width = p[1]
+        height = p[0]
 
-                if (bool == False):
-                    return False
-                return True
+        widthM = pM[1]
+        heightM = pM[0]
 
-            if center == 2:
-                bool = \
-                    img[I, J + 1] == threshold \
-                    or img[I, J - 1] == threshold \
-                    or img[I + 1, J] == threshold \
-                    or img[I - 1, J] == threshold \
-                    or img[I - 1, J - 1] == threshold \
-                    or img[I - 1, J + 1] == threshold \
-                    or img[I + 1, J - 1] == threshold \
-                    or img[I + 1, J + 1] == threshold \
-                    or img[I, J + 2] == threshold \
-                    or img[I, J - 2] == threshold \
-                    or img[I + 2, J] == threshold \
-                    or img[I - 2, J] == threshold \
-                    or img[I - 1, J - 2] == threshold \
-                    or img[I - 1, J + 2] == threshold \
-                    or img[I + 2, J - 2] == threshold \
-                    or img[I + 2, J + 2] == threshold \
-                    or img[I + 2, J - 1] == threshold \
-                    or img[I + 2, J + 1] == threshold \
-                    or img[I + 1, J + 2] == threshold \
-                    or img[I + 1, J - 2] == threshold \
-                    or img[I - 2, J + 2] == threshold \
-                    or img[I - 2, J - 2] == threshold \
-                    or img[I - 2, J + 1] == threshold \
-                    or img[I - 2, J - 1] == threshold
+        centerHeightM = int(np.ceil(heightM / 2) - 1)
+        centerwidthM = int(np.ceil(widthM / 2) - 1)
+        img2 = np.zeros((height + centerHeightM * 2, width + centerwidthM * 2), np.uint8)
+        img2[centerHeightM:height + centerHeightM, centerwidthM:width + centerwidthM] = img
 
-                if (bool == False):
-                    return False
-                return True
+        for i in range(heightM):
+            for j in range(widthM):
+                if mask[i, j] == 1:
+                    mask[i, j] = 255
+        return width,height,centerHeightM,centerwidthM,img2, mask,
+
+
 
     @staticmethod
     def dilate(img, mask):
-        p = img.shape
-        width = p[1]
-        height = p[0]
-        n = len(mask) - 1
-
-        center = int(np.ceil(len(mask) / 2) - 1)
-        img2 = np.zeros((height + center * 2, width + center * 2))
-        img2[center:height + center, center:width + center] = img
+        width,height,centerHeightM,centerwidthM,img2, mask = cv3.preparation(img,mask)
         img3 = np.copy(img2)
 
-
-        if (mask[0][0] == 0 and mask[0][n] == 0 and mask[n][0] == 0 and  mask[n][n] == 0):
-            type = 'cross'
-        else:
-            type = 'square'
-
-        for i in range(n + 1):
-            for j in range(n + 1):
-                if mask[i][j] == 1:
-                    mask[i][j] = 255
-
-
-        for i in range(center, height + center):
-            for j in range(center, width + center):
-                if (cv3.dilateIsTrue(type, img2, i, j, center)):
+        for i in range(centerHeightM, height + centerHeightM):
+            for j in range(centerwidthM, width + centerwidthM):
+                chunk = img2[i - centerHeightM: i + centerHeightM + 1, j - centerwidthM: j + centerwidthM + 1]
+                if (cv3.dilateIsTrue(chunk, mask, centerHeightM,centerwidthM)):
                     img3[i, j] = 255
                 else:
                     img3[i, j] = 0
+        return img3[centerHeightM:height + centerHeightM, centerwidthM:width + centerwidthM]
 
-        return  img3[center:height + center, center:width + center]
 
     @staticmethod
     def erode(img, mask):
-        p = img.shape
-        width = p[1]
-        height = p[0]
-        n = len(mask) - 1
-
-        center = int(np.ceil(len(mask) / 2) - 1)
-        img2 = np.zeros((height + center * 2, width + center * 2))
-        img2[center:height + center, center:width + center] = img
+        width, height, centerHeightM, centerwidthM, img2, mask = cv3.preparation(img, mask)
         img3 = np.copy(img2)
 
-        if (mask[0][0] == 0 and mask[0][n] == 0 and mask[n][0] == 0 and mask[n][n] == 0):
-            type = 'cross'
-        else:
-            type = 'square'
-
-        for i in range(n + 1):
-            for j in range(n + 1):
-                if mask[i][j] == 1:
-                    mask[i][j] = 255
-
-        for i in range(center, height + center):
-            for j in range(center, width + center):
-                if (cv3.dilateIsTrue(type, img2, i, j, center)):
+        for i in range(centerHeightM, height + centerHeightM):
+            for j in range(centerwidthM, width + centerwidthM):
+                chunk = img2[i - centerHeightM: i + centerHeightM + 1, j - centerwidthM: j + centerwidthM + 1]
+                if (cv3.erodeIsTrue(chunk, mask, centerHeightM, centerwidthM)):
                     img3[i, j] = 255
                 else:
                     img3[i, j] = 0
-
-        return img3
+        return img3[centerHeightM:height + centerHeightM, centerwidthM:width + centerwidthM]
 
     @staticmethod
     def open(img, mask):
@@ -229,13 +133,7 @@ class cv3:
     @staticmethod
     def contour(img, mask):
         dilation = cv3.dilate(img, mask).astype(np.uint8)
-        p1 = img.shape[0]
-        p2 = img.shape[1]
-        img2 = img
-        for i in range(p1):
-            for j in range(p2):
-                img2[i, j] = operator.xor(dilation[i, j], img[i, j])
-        return img2
+        return dilation ^ img
 
     @staticmethod
     def threshold_processing(img, threshold):
@@ -256,13 +154,17 @@ if __name__ == '__main__':
     img = cv2.imread("rab2.jpg", 0)
 
     img = cv3.threshold_processing(img, 195)
-    out1 = cv3.addNoise(img, 99)
+    out1 = cv3.addNoise(img, 1)
     cv2.imshow(' out1', out1)
 
-    kernel = np.ones((5, 5), np.uint8)
+    kernel = np.ones((3,3),np.uint8)
+    my = cv3.erode(out1,kernel)
 
-    im = cv3.contour(img,kernel)
-    cv2.imshow('im', im)
+    cv2.imshow('my', my)
+
+    st = cv2.erode(out1, kernel)
+    cv2.imshow('st', st)
+
+    cv2.imshow('xor', st^my)
     cv2.waitKey(0)
-
 
