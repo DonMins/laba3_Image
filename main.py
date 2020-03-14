dd7fa30d7f42060eb6a2180e0670b32fa3b3c7d7
DonMins <maks000156@yandex.ru>
2020-03-14 11:49:18 +0400

    я это делать больше не буду

--- main.py
+++ main.py
@@ -1,191 +1,268 @@
 import operator
-
 import  cv2
 import numpy as np
 
-def addNoise(im,noise_percentage):
-    vals = len(im.flatten())
-    p = img.shape
-    width = p[1]
-    height = p[0]
-    listAllCoord = []
-    for i in range(height):
-        for j in range(width):
-            listAllCoord.append([i,j])
-    out = np.copy(im)
-    num_salt = int(np.ceil(noise_percentage * vals /100))
+class cv3:
+    def __init__(self):
+        pass
 
-    for i in range(num_salt):
-        # coord = listAllCoord.pop(np.random.randint(0, len(listAllCoord) - 1))
-        coord = [np.random.randint(0, i - 1) for i in im.shape]
-
-        if img[coord[0],coord[1]] == 255:
-            out[coord[0], coord[1]] = 0
-        else :
-            out[coord[0],coord[1]] = 255
-    return out
-
-
-def noise_percentage(img,img2):
-    p = img.shape
-    width = p[1]
-    height = p[0]
-    countWhite1 = 0
-    countBlack1 = 0
-    countWhite2 = 0
-    countBlack2 = 0
-
-    for i in range(height):
-        for j in range(width):
-            if (img[i, j] == 255):
-                countWhite1 += 1
-            else:
-                countBlack1+=1
-            if (img2[i, j] == 255):
-                countWhite2 += 1
-            else:
-                countBlack2+=1
-
-
-    print("Отношение : " ,
-          100*((abs(countWhite1 - countWhite2) + abs(countBlack1 - countBlack2))/ (width * height))
-)
-
-
-
-def threshold_processing(img, threshold):
-    p = img.shape
-    width = p[1]
-    height = p[0]
-
-    for i in range(height):
-        for j in range(width):
-            if (img[i, j] > threshold):
-                img[i, j] = 255
-            else:
-                img[i, j] = 0
-
-    return img
-
-
-def morphology(img, type, sizeMask, typeMask):
-    p = img.shape
-    width = p[1]
-    height = p[0]
-# нужно убрать говнокод и сделать обработку границ , при изменение размера маски, в конце он будет выходить за границы картинки
-    def erosion(size, type, img, I, J, threshold):
-        if type == "cross":
-            center = int (np.ceil(size / 2) - 1)
+    @classmethod
+    def erodeIsTrue(self,type, img, I, J , center):
+        threshold = 255
+        if type == 'cross':
             for i in range(1, center + 1):
-                bool = img[I, J + i] == threshold and img[I, J - i] == threshold and img[I + i, J] == threshold and img[
-                    I - i, J] == threshold
+                bool = img[I, J + i] == threshold and img[I, J - i] == threshold and img[I + i, J] == threshold and \
+                       img[I - i, J] == threshold
                 if (bool == False):
                     return False
             return True
-        if type == "square":
-            center = int(np.ceil(size / 2) - 1)
-            for i in range(1, center + 1):
-                for j in range(1, center + 1):
-                    bool = img[I, J + j] == threshold and img[I, J - j] == threshold and img[I + i, J] == threshold and img[
-                    I - i, J] == threshold and img[I - i, J - j] == threshold and img[I - i, J + j] == threshold and\
-                       img[I + i, J - j] == threshold and img[I + i, J + j] == threshold
-                    if (bool == False):
-                        return False
-            return True
 
-    def dilation(size, type, img, I, J, threshold):
-        if type == "cross":
-            center = int(np.ceil(size / 2) - 1)
+        if type == "square":
+            if center == 1:
+                bool = img[I, J + 1] == threshold and  img[I, J - 1] == threshold and img[
+                    I + 1, J] == threshold and img[
+                           I - 1, J] == threshold or img[I - 1, J - 1] == threshold and img[
+                           I - 1, J + 1] == threshold and \
+                       img[I + 1, J - 1] == threshold and img[I + 1, J + 1] == threshold
+                if (bool == False):
+                    return False
+                return True
+
+            if center == 2:
+                bool = \
+                    img[I, J + 1] == threshold \
+                    and img[I, J - 1] == threshold \
+                    and img[I + 1, J] == threshold \
+                    and img[I - 1, J] == threshold \
+                    and img[I - 1, J - 1] == threshold \
+                    and img[I - 1, J + 1] == threshold \
+                    and img[I + 1, J - 1] == threshold \
+                    and img[I + 1, J + 1] == threshold \
+                    and img[I, J + 2] == threshold \
+                    and img[I, J - 2] == threshold \
+                    and img[I + 2, J] == threshold \
+                    and img[I - 2, J] == threshold \
+                    and img[I - 1, J - 2] == threshold \
+                    and img[I - 1, J + 2] == threshold \
+                    and img[I + 2, J - 2] == threshold \
+                    and img[I + 2, J + 2] == threshold \
+                    and img[I + 2, J - 1] == threshold \
+                    and img[I + 2, J + 1] == threshold \
+                    and img[I + 1, J + 2] == threshold \
+                    and img[I + 1, J - 2] == threshold \
+                    and img[I - 2, J + 2] == threshold \
+                    and img[I - 2, J - 2] == threshold \
+                    and img[I - 2, J + 1] == threshold \
+                    and img[I - 2, J - 1] == threshold
+
+                if (bool == False):
+                    return False
+                return True
+
+    @classmethod
+    def dilateIsTrue(self,type, img, I, J , center):
+        threshold = 255
+        if type == 'cross':
             for i in range(1, center + 1):
                 bool = img[I, J + i] == threshold or img[I, J - i] == threshold or img[I + i, J] == threshold or \
-                       img[ I - i, J] == threshold
+                       img[I - i, J] == threshold
                 if (bool == False):
                     return False
             return True
+
         if type == "square":
-            center = int(np.ceil(size / 2) - 1)
-            for i in range(1, center + 1):
-                for j in range(1, center + 1):
-                    bool = img[I, J + j] == threshold or img[I, J - j] == threshold or img[
-                        I + i, J] == threshold or img[
-                               I - i, J] == threshold or img[I - i, J - j] == threshold or img[
-                               I - i, J + j] == threshold or \
-                           img[I + i, J - j] == threshold or img[I + i, J + j] == threshold
-                    if (bool == False):
-                        return False
-            return True
+            if center == 1:
+                bool = img[I, J + 1] == threshold or img[I, J - 1] == threshold or img[
+                    I + 1, J] == threshold or img[
+                           I - 1, J] == threshold or img[I - 1, J - 1] == threshold or img[
+                           I - 1, J + 1] == threshold or\
+                       img[I + 1, J - 1] == threshold or img[I + 1, J + 1] == threshold
 
-    center = int(np.ceil(sizeMask / 2) - 1)
-    img2 = np.zeros(( height + center*2, width + center*2,))
-    img2[center:height+center,center:width+center] = img
-    img3 = np.copy(img2)
+                if (bool == False):
+                    return False
+                return True
 
-    for i in range(0, height):
-        for j in range(0, width):
-            if type == "erosion":
-                if (erosion(sizeMask, typeMask, img2, i, j, 255)):
+            if center == 2:
+                bool = \
+                    img[I, J + 1] == threshold \
+                    or img[I, J - 1] == threshold \
+                    or img[I + 1, J] == threshold \
+                    or img[I - 1, J] == threshold \
+                    or img[I - 1, J - 1] == threshold \
+                    or img[I - 1, J + 1] == threshold \
+                    or img[I + 1, J - 1] == threshold \
+                    or img[I + 1, J + 1] == threshold \
+                    or img[I, J + 2] == threshold \
+                    or img[I, J - 2] == threshold \
+                    or img[I + 2, J] == threshold \
+                    or img[I - 2, J] == threshold \
+                    or img[I - 1, J - 2] == threshold \
+                    or img[I - 1, J + 2] == threshold \
+                    or img[I + 2, J - 2] == threshold \
+                    or img[I + 2, J + 2] == threshold \
+                    or img[I + 2, J - 1] == threshold \
+                    or img[I + 2, J + 1] == threshold \
+                    or img[I + 1, J + 2] == threshold \
+                    or img[I + 1, J - 2] == threshold \
+                    or img[I - 2, J + 2] == threshold \
+                    or img[I - 2, J - 2] == threshold \
+                    or img[I - 2, J + 1] == threshold \
+                    or img[I - 2, J - 1] == threshold
+
+                if (bool == False):
+                    return False
+                return True
+
+    @staticmethod
+    def dilate(img, mask):
+        p = img.shape
+        width = p[1]
+        height = p[0]
+        n = len(mask) - 1
+
+        center = int(np.ceil(len(mask) / 2) - 1)
+        img2 = np.zeros((height + center * 2, width + center * 2))
+        img2[center:height + center, center:width + center] = img
+        img3 = np.copy(img2)
+
+
+        if (mask[0][0] == 0 and mask[0][n] == 0 and mask[n][0] == 0 and  mask[n][n] == 0):
+            type = 'cross'
+        else:
+            type = 'square'
+
+        for i in range(n + 1):
+            for j in range(n + 1):
+                if mask[i][j] == 1:
+                    mask[i][j] = 255
+
+
+        for i in range(center, height + center):
+            for j in range(center, width + center):
+                if (cv3.dilateIsTrue(type, img2, i, j, center)):
                     img3[i, j] = 255
                 else:
                     img3[i, j] = 0
 
-            if type == "dilation":
-                if (dilation(sizeMask, typeMask, img2, i, j, 255)):
+        return  img3[center:height + center, center:width + center]
+
+    @staticmethod
+    def erode(img, mask):
+        p = img.shape
+        width = p[1]
+        height = p[0]
+        n = len(mask) - 1
+
+        center = int(np.ceil(len(mask) / 2) - 1)
+        img2 = np.zeros((height + center * 2, width + center * 2))
+        img2[center:height + center, center:width + center] = img
+        img3 = np.copy(img2)
+
+        if (mask[0][0] == 0 and mask[0][n] == 0 and mask[n][0] == 0 and mask[n][n] == 0):
+            type = 'cross'
+        else:
+            type = 'square'
+
+        for i in range(n + 1):
+            for j in range(n + 1):
+                if mask[i][j] == 1:
+                    mask[i][j] = 255
+
+        for i in range(center, height + center):
+            for j in range(center, width + center):
+                if (cv3.dilateIsTrue(type, img2, i, j, center)):
                     img3[i, j] = 255
                 else:
                     img3[i, j] = 0
 
-    return img3[center:height + center,  center:width + center]
+        return img3
+
+    @staticmethod
+    def open(img, mask):
+        img2 = cv3.erode(img, mask)
+        return cv3.dilate(img2, mask)
+
+    @staticmethod
+    def close(img, mask):
+        img2 = cv3.dilate(img, mask)
+        return cv3.erode(img2, mask)
+
+
+    @staticmethod
+    def addNoise(im, noise_percentage):
+        vals = len(im.flatten())
+        p = img.shape
+        width = p[1]
+        height = p[0]
+        listAllCoord = []
+        for i in range(height):
+            for j in range(width):
+                listAllCoord.append([i, j])
+        out = np.copy(im)
+        num_salt = int(np.ceil(noise_percentage * vals / 100))
+
+        for i in range(num_salt):
+            coord = [np.random.randint(0, i - 1) for i in im.shape]
+
+            if img[coord[0], coord[1]] == 255:
+                out[coord[0], coord[1]] = 0
+            else:
+                out[coord[0], coord[1]] = 255
+
+        return out
+
+    @staticmethod
+    def noise_percentage(img, img2):
+        p = img.shape
+        width = p[1]
+        height = p[0]
+        img3 = img
+        countNoise = 0
+        for i in range(height):
+            for j in range(width):
+                if (operator.xor(img2[i, j], img[i, j])) == 255:
+                    countNoise += 1
+
+        print("Отношение : ", countNoise * 100 / (width * height))
+        return countNoise * 100 / (width * height)
+
+    @staticmethod
+    def contour(img, mask):
+        dilation = cv3.dilate(img, mask).astype(np.uint8)
+        p1 = img.shape[0]
+        p2 = img.shape[1]
+        img2 = img
+        for i in range(p1):
+            for j in range(p2):
+                img2[i, j] = operator.xor(dilation[i, j], img[i, j])
+        return img2
+
+    @staticmethod
+    def threshold_processing(img, threshold):
+        p = img.shape
+        width = p[1]
+        height = p[0]
+
+        for i in range(height):
+            for j in range(width):
+                if (img[i, j] > threshold):
+                    img[i, j] = 255
+                else:
+                    img[i, j] = 0
+        return img
 
 if __name__ == '__main__':
 
     img = cv2.imread("rab2.jpg", 0)
-    cv2.imshow('Input image', img)
 
-    img = threshold_processing(img,195)
-    cv2.imshow('Input image', img)
+    img = cv3.threshold_processing(img, 195)
+    out1 = cv3.addNoise(img, 99)
+    cv2.imshow(' out1', out1)
 
-    out1 = addNoise(img,1)
-    cv2.imshow("noise1%.jpg", out1)
-    #
-    # out2 = addNoise(img,2 )
-    # cv2.imshow("noise2%.jpg", out2)
-    #
-    # out5 = addNoise(img,5 )
-    # cv2.imshow("noise5%.jpg", out5)
-    #
-    # out10 = addNoise(img,10 )
-    # cv2.imshow("noise10%.jpg", out10)
-    #
-    # out20 = addNoise(img, 20)
-    # cv2.imshow("noise20%.jpg", out20)
-    #
-    out50 = addNoise(img,50 )
-    cv2.imshow("noise50%.jpg", out50)
+    kernel = np.ones((5, 5), np.uint8)
 
-    av  = morphology(out50, "erosion", 3, "cross")
-    #
-    #
-    #--------------------Контура---------------------------
-
-    # img = cv2.imread("rec.jpg",0)
-    # img = threshold_processing(img,195)
-    # cv2.imshow('Input image', img)
-    #
-    # cv2.imshow("dilation1.jpg", morphology(img, "dilation1", 7, "square"))
-    # out1 = addNoise(img, 50)
-    # cv2.imshow("noise50%.jpg", out1)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-    img = cv2.imread("rab2Gr.jpg", 0)
-    cv2.imshow('Input image', img)
-    kernel = np.ones((3, 3), np.uint8)
-    dilation = cv2.dilate(img, kernel, iterations=1)
-    p1=img.shape[0]
-    p2=img.shape[1]
-    img2=img
-    for i in range (p1):
-        for j in range(p2):
-            img2[i,j]= operator.xor(dilation[i,j], img[i,j])
-    cv2.imshow(' image', img2)
+    im = cv3.contour(img,kernel)
+    cv2.imshow('im', im)
     cv2.waitKey(0)
+
 
